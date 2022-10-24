79d915d53feb649d5f7d2d702b5ac189b40784de
bono-94 <bono.94@hotmail.com>
2022-10-24 14:52:40 +0000

    Create first functions structure

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+function startGame () {
+
+}
+
+function runGame () {
+    
+}
+
+function checkReaction () {
+
+}
+
+function calculateReaction () {
+
+}
+
+function calculateBalance () {
+
+}
+
+function calculateInvestment () {
+
+}
+
+function calculateEarnings () {
+
+}
+
+function recordScore () {
+
+}
+
+function updateScoreWin () {
+
+}
+
+function updateScoreLoss () {
+
+}
+
+function opponentReaction () {
+    
+}
